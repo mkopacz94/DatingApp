027b8f916621ec2555c0a695b5332773e4f5a176
mkopacz94 <m.kopacz94@gmail.com>
2020-04-27 08:43:18 +0200

    End of section 3

--- DatingApp.API/DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/DatingApp.API/Data/DataContext.cs
@@ -10,6 +10,8 @@ namespace DatingApp.API.Data
     public class DataContext : DbContext
     {
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
     }
 }
